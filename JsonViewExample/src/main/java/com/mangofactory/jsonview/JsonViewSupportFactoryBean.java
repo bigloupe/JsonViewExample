2c00cb7b52dcb80e0f3c8b34b9c30fff144b03a5
m329997 <cetorcq@airfrance.fr>
2014-04-07 16:15:58 +0000

    Support Spring 4.x and Jackson 2.x

--- JsonViewExample/src/main/java/com/mangofactory/jsonview/JsonViewSupportFactoryBean.java
+++ JsonViewExample/src/main/java/com/mangofactory/jsonview/JsonViewSupportFactoryBean.java
@@ -1,48 +1,56 @@
 package com.mangofactory.jsonview;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import lombok.extern.slf4j.Slf4j;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandlerComposite;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
-import com.google.common.collect.Lists;
-
 /**
- * Modified Spring 3.1's internal Return value handlers, and wires up a decorator
- * to add support for @JsonView
+ * Modified Spring 3.1's internal Return value handlers, and wires up a
+ * decorator to add support for @JsonView
  * 
  * @author martypitt
- *
+ * 
  */
-@Slf4j
 public class JsonViewSupportFactoryBean implements InitializingBean {
 
-    @Autowired
-    private RequestMappingHandlerAdapter adapter;
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        HandlerMethodReturnValueHandlerComposite returnValueHandlers = adapter.getReturnValueHandlers();
-        List<HandlerMethodReturnValueHandler> handlers = Lists.newArrayList(returnValueHandlers.getHandlers());
-        decorateHandlers(handlers);
-        adapter.setReturnValueHandlers(handlers);
-    }
-    private void decorateHandlers(List<HandlerMethodReturnValueHandler> handlers) {
-        for (HandlerMethodReturnValueHandler handler : handlers) {
-            if (handler instanceof RequestResponseBodyMethodProcessor)
-            {
-                ViewInjectingReturnValueHandler decorator = new ViewInjectingReturnValueHandler(handler);
-                int index = handlers.indexOf(handler);
-                handlers.set(index, decorator);
-                log.info("JsonView decorator support wired up");
-                break;
-            }
-        }        
-    }
+	private final Logger log = LoggerFactory
+			.getLogger(JsonViewSupportFactoryBean.class);
+
+	@Autowired
+	private RequestMappingHandlerAdapter adapter;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		List myHandlers = new ArrayList();
+		for (HandlerMethodReturnValueHandler handler : adapter
+				.getReturnValueHandlers()) {
+			if (handler instanceof RequestResponseBodyMethodProcessor) {
+				ViewInjectingReturnValueHandler decorator = new ViewInjectingReturnValueHandler(
+						handler);
+				myHandlers.add(decorator);
+			} else
+				myHandlers.add(handler);
+		}
+		adapter.setReturnValueHandlers(myHandlers);
+	}
+
+	private void decorateHandlers(List<HandlerMethodReturnValueHandler> handlers) {
+		List customList = new ArrayList();
+		for (HandlerMethodReturnValueHandler handler : handlers) {
+			if (handler instanceof RequestResponseBodyMethodProcessor) {
+				handler = new ViewInjectingReturnValueHandler(handler);
+				log.info("JsonView decorator support wired up");
+			}
+			customList.add(handler);
+		}
+		adapter.setReturnValueHandlers(customList);
+	}
 
 }
--- JsonViewExample/src/main/java/com/mangofactory/jsonview/ViewInjectingReturnValueHandler.java
+++ JsonViewExample/src/main/java/com/mangofactory/jsonview/ViewInjectingReturnValueHandler.java
@@ -1,60 +1,73 @@
 package com.mangofactory.jsonview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 /**
-* Decorator that detects a declared {@link ResponseView}, and 
-* injects support if required
-* @author martypitt
-*
-*/
+ * Decorator that detects a declared {@link ResponseView}, and injects support
+ * if required
+ * 
+ * @author martypitt
+ * 
+ */
 public class ViewInjectingReturnValueHandler implements
-       HandlerMethodReturnValueHandler {
+		HandlerMethodReturnValueHandler {
 
-   private final HandlerMethodReturnValueHandler delegate;
+	private final HandlerMethodReturnValueHandler delegate;
 
-   public ViewInjectingReturnValueHandler(HandlerMethodReturnValueHandler delegate)
-   {
-       this.delegate = delegate;
-   }
-   @Override
-   public boolean supportsReturnType(MethodParameter returnType) {
-       return delegate.supportsReturnType(returnType);
-   }
+	public ViewInjectingReturnValueHandler(
+			HandlerMethodReturnValueHandler delegate) {
+		this.delegate = delegate;
+	}
 
-   @Override
-   public void handleReturnValue(Object returnValue,
-           MethodParameter returnType, ModelAndViewContainer mavContainer,
-           NativeWebRequest webRequest) throws Exception {
 
-       Class<? extends BaseView> viewClass = getDeclaredViewClass(returnType);
-       if (viewClass != null)
-       {
-           returnValue = wrapResult(returnValue,viewClass);    
-       }
+	@Override
+	public boolean supportsReturnType(MethodParameter returnType) {
+		return delegate.supportsReturnType(returnType);
+	}
 
-       delegate.handleReturnValue(returnValue, returnType, mavContainer, webRequest);
-   }
-   /**
-    * Returns the view class declared on the method, if it exists.
-    * Otherwise, returns null.
-    * @param returnType
-    * @return
-    */
-   private Class<? extends BaseView> getDeclaredViewClass(MethodParameter returnType) {
-       ResponseView annotation = returnType.getMethodAnnotation(ResponseView.class);
-       if (annotation != null)
-       {
-           return annotation.value();
-       } else {
-           return null;
-       }
-   }
-   private Object wrapResult(Object result, Class<? extends BaseView> viewClass) {
-       PojoView response = new PojoView(result, viewClass);
-       return response;
-   }
+	@Override
+	public void handleReturnValue(Object returnValue,
+			MethodParameter returnType, ModelAndViewContainer mavContainer,
+			NativeWebRequest webRequest) throws Exception {
+
+		Class<? extends BaseView> viewClass = getDeclaredViewClass(returnType);
+		if (viewClass != null) {
+			returnValue = wrapResult(returnValue, viewClass);
+		}
+
+		delegate.handleReturnValue(returnValue, returnType, mavContainer,
+				webRequest);
+	}
+
+	/**
+	 * Returns the view class declared on the method, if it exists. Otherwise,
+	 * returns null.
+	 * 
+	 * @param returnType
+	 * @return
+	 */
+	private Class<? extends BaseView> getDeclaredViewClass(
+			MethodParameter returnType) {
+		ResponseView annotation = returnType
+				.getMethodAnnotation(ResponseView.class);
+		if (annotation != null) {
+			return annotation.value();
+		} else {
+			return null;
+		}
+	}
+
+	private Object wrapResult(Object result, Class<? extends BaseView> viewClass) {
+		PojoView response = new PojoView(result, viewClass);
+		return response;
+	}
+
+
 }
